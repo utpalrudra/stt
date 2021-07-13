03927b72afac7d41eb993fbd8171877460e62642
Utpal Rudra <80615328+utpalrudra@users.noreply.github.com>
2021-07-13 12:59:59 +0600

    Add files via upload

---
+++ removing_certain_files.py
@@ -0,0 +1,9 @@
+import os
+directory = "/media/utpal/Software/03"
+
+files_in_directory = os.listdir(directory)
+filtered_files = [file for file in files_in_directory if file.endswith(".flac")]
+
+for file in filtered_files:
+	path_to_file = os.path.join(directory, file)
+	os.remove(path_to_file)
